e406b6ee52d9baa827b437654ef772ece7d54d5f
EnginYelgen <enginyelgen@hotmail.com>
2015-01-14 22:56:17 +0200

    nodeJS samples

---
+++ NodejsApp_SQLConnection/server.js
@@ -0,0 +1,166 @@
+﻿/*
+// Connection test 1
+
+var sql = require('mssql');
+
+var config = {
+    user: 'guest',
+    password: '123',
+    server: 'ENG\\SQLEXPRESS', // You can use 'localhost\\instance' to connect to named instance
+    database: 'NodeJS',
+    port: 1433,
+    options: {
+        //instanceName: 'SQLEXPRESS',
+        encrypt: false // Use this if you're on Windows Azure
+    }
+}
+
+var connection = new sql.Connection(config);
+
+connection.connect(function (err) {
+    if (err) {
+        console.dir(err);
+    }
+});
+
+var request = new sql.Request(connection);
+
+var query = 'SELECT * FROM Sample_1';
+
+request.query(query, function (err, recordset) {
+    if (err) {
+        console.dir(err);
+    }
+    else {
+        console.dir(recordset);
+    }
+});
+
+connection.close();
+*/
+
+
+
+// Connection test 2
+
+var sql = require('mssql');
+
+var config = {
+    user: 'guest',
+    password: '123',
+    //server: '178.245.139.4', // You can use 'localhost\\instance' to connect to named instance
+    //server: 'localhost\\SQLEXPRESS',
+    server: 'localhost',
+    //server: 'ENG',
+    //server: '178.245.239.18',
+    //server: '127.0.0.1',
+    //server: '127.0.0.1\\SQLEXPRESS',
+    database: 'NodeJS',
+    //port: 1433,
+    //driver: 'tedious',
+    //connectionTimeout: 10000,
+    //requestTimeout: 10000,
+    options: {
+        instanceName: 'SQLEXPRESS',
+        encrypt: false // Use this if you're on Windows Azure
+    }
+    //, pool: {
+    //    idleTimeoutMillis: 10000,
+    //    max: 100
+    //}
+    //,debug:
+    //{
+    //    packet: true,
+    //    data: true,
+    //    payload: true,
+    //    token: true,
+    //    log: true
+    //}
+}
+
+var connection = new sql.Connection(config, function (err) {
+    // ... error checks
+
+    if (err) {
+        console.dir(err);
+    }
+    else {
+
+        // Query
+
+        var request = new sql.Request(connection); // or: var request = connection.request();
+        request.query('select 1 as number', function (err, recordset) {
+            // ... error checks
+            if (err) {
+                console.dir(err);
+            }
+            else {
+                console.dir(recordset);
+            }
+        });
+
+        // Stored Procedure
+
+        //var request = new sql.Request(connection);
+        //request.input('input_parameter', sql.Int, 10);
+        //request.output('output_parameter', sql.VarChar(50));
+        //request.execute('procedure_name', function (err, recordsets, returnValue) {
+        //    // ... error checks
+
+        //    console.dir(recordsets);
+        //});
+    }
+});
+
+/*
+// Connection test 3
+
+var Connection = require('tedious').Connection;
+
+var config = {
+    userName: 'guest',
+    password: '123',
+    //server: '127.0.0.1',
+    server: 'localhost',
+    // If you're on Windows Azure, you will need this:
+    options:
+    {
+        encrypt: false,
+        instanceName: 'SQLEXPRESS',
+        database: 'NodeJS'
+    }
+};
+
+var connection = new Connection(config);
+
+connection.on('connect', function (err) {
+    // If no error, then good to go...
+    if (err) {
+        console.dir(err);
+    }
+    else {
+        executeStatement();
+    }
+}
+);
+
+var Request = require('tedious').Request;
+
+function executeStatement() {
+    request = new Request("select 42, 'hello world'", function (err, rowCount) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(rowCount + ' rows');
+        }
+    });
+
+    request.on('row', function (columns) {
+        columns.forEach(function (column) {
+            console.log(column.value);
+        });
+    });
+
+    connection.execSql(request);
+}
+*/
---
+++ WebApplication/server.js
@@ -0,0 +1,50 @@
+﻿// HTML Controls
+    
+//var app = require('express')();
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+
+//app.get('/', function (req, res) {
+//    res.sendfile('HtmlControls.html');
+//});
+
+//io.on('connection', function (socket) {
+//    socket.on('text change', function (msg) {
+//        io.emit('text change', msg);
+//    });
+//});
+
+//http.listen(3000, function () {
+//    console.log('listening on *:3000');
+//});
+
+
+// Page Visiter Numbers
+var count = 0;
+
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+    // routing
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/PageView.html');
+});
+
+io.sockets.on('connection', function (socket) {
+
+    // when the client emits 'adduser', this listens and executes
+    socket.on('PageLoad', function () {
+        count++;
+        io.sockets.emit('CurrentVisitorCount', count);
+    });
+
+    socket.on('disconnect', function () {
+        count--;
+        io.sockets.emit('PageDisconnect', count);
+    });
+});
